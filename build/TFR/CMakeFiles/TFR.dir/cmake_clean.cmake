314c4680d8a0ac34aefe168c39142e0ac2b09b9d
Sahil Surapaneni <sahil.surapaneni18@gmail.com>
2023-03-04 19:06:57 -0500

    Add files via upload

---
+++ build/TFR/CMakeFiles/TFR.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TFR.dir/TFR.cpp.o"
+  "CMakeFiles/TFR.dir/TFR.cpp.o.d"
+  "TFR.pdb"
+  "TFR.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TFR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/TFR/CMakeFiles/TFR.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TFR.
+# This may be replaced when dependencies are built.
